cf145dd7789c4715506162c36e321252fcc00388
Anu Olusanya <ilorianuoluwapo@gmail.com>
2022-12-09 10:03:22 +0100

    reduced  image opacity

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -6,47 +6,47 @@ import './ImageSlider.css';
 
 const ImageSlider = () => {
   
-    const settings = {
-      infinite: true,
-      dots: false,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      lazyLoad: true,
-      autoplay: true,
-    autoplaySpeed: 2000,
-    responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-           slidesToShow: 1,
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-           slidesToShow: 1,
-          }
-         }
-      ]
+  const settings = {
+    infinite: true,
+    dots: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    lazyLoad: true,
+    autoplay: true,
+  autoplaySpeed: 2000,
+  responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+         slidesToShow: 1,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+         slidesToShow: 1,
+        }
+       }
+    ]
 
-    };
+  };
 
 
-    return (
-      <>
-        <div className="imgslider">
-          <Slider {...settings}>
-            {images.map((item) => (
-              <div key={item.id}>
-                <img src={item.src}  alt={item.alt} />
-              </div>
-            ))}
-            
-          </Slider>
-        </div>
-            </>
-    )
-  
-  
-   }  
-   export default ImageSlider;
+  return (
+    <>
+      <div className="imgslider">
+        <Slider {...settings}>
+          {images.map((item) => (
+            <div key={item.id}>
+              <img src={item.src}  alt={item.alt} />
+            </div>
+          ))}
+          
+        </Slider>
+      </div>
+          </>
+  )
+
+
+ }  
+ export default ImageSlider;
